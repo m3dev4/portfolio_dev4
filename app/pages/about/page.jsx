75fa12b69d3ea6309b3f9c59a288938cae4d15df
m3dev4 <m3dev4@gmail.com>
2024-10-09 13:56:34 +0000

    test

--- app/pages/about/page.jsx
+++ app/pages/about/page.jsx
@@ -1,19 +1,27 @@
-import StickyScroll from '@/components/stickyScroll'
-import React from 'react'
+import Header from "@/components/header/header";
+import Skills from "@/components/skill";
+import StickyScroll from "@/components/stickyScroll";
+import Link from "next/link";
+import React from "react";
 
 const About = () => {
   return (
-   <section>
-    <div className='h-screen w-full bg-red-500 flex items-center justify-center'>
-      <h1 className='text-indigo-700 font-extrabold text-3xl'>
-        content
-      </h1>
-    </div>
-    <div className='relative h-screen w-full'>
-      <StickyScroll />
-    </div>
-   </section>
-  )
-}
+    <section className="w-full h-full block ">
+      <div className="bg-blue-400 flex py-8 px-11 fixed z-10 items-center justify-between w-full">
+        <Link href="/" className="cursor-pointer text-2xl font-extrabold uppercase">
+          M3dev4
+        </Link>
+        <Header />
+      </div>
+      <div className="h-screen w-full bg-red-500 flex items-center justify-center"></div>
+      <div className=" h-[300vh] w-full">
+        <StickyScroll />    
+      </div>
+      <div className="relative w-full">
+        <Skills />
+      </div>
+    </section>
+  );
+};
 
-export default About
+export default About;
